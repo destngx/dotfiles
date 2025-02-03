5f870ec987535814912bf29343f07cfac05aa114
destngx <chromevi123@gmail.com>
2025-02-03 19:13:50 +0700

    feat(raycast): add youtube download mp3 script

---
+++ raycast/youtube-download-mp3.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Youtube Download MP3
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }
+# @raycast.packageName ytmp3
+
+# Documentation:
+# @raycast.description Youtube download mp3
+# @raycast.author destnguyxn
+# @raycast.authorURL https://raycast.com/destnguyxn
+
+ytmp3 $1
+
